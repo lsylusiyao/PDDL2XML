d5f5eeba132a9de3f55e8b3067c13fa6636728e1
lsylusiyao <lsylusiyao@qq.com>
2019-05-12 12:29:53 +0800

    update

--- RWxml/ReadFile.cpp
+++ RWxml/ReadFile.cpp
@@ -1,10 +1,69 @@
 #include "ReadFile.h"
+using namespace std;
+
 
 ReadFileClass::ReadFileClass(const char* path)
 {
-	std::ifstream in(path);
+	ifstream in(path);
 }
 
 ReadFileClass::~ReadFileClass()
 {
+
+}
+
+void ReadFileClass::RealAll()
+{
+	regex notBlankR("\\S");
+	regex noteR("//.*");
+	int lineNum = 0; //存储中的行号
+	while (getline(in,eachLine))
+	{
+		regex_replace(eachLine, noteR, ""); //删除所有备注
+		if (!regex_match(eachLine, notBlankR)) //true表示一行只有空格和回车（不可见字符），即删除所有空行
+		{
+			eachLine = "";
+			continue;
+		}
+		if (eachLine.find("domain")) partNum["domain"] = lineNum;
+		if (eachLine.find("requirements")) partNum["requirements"] = lineNum;
+		if (eachLine.find("types")) partNum["types"] = lineNum;
+		if (eachLine.find("predicates")) partNum["predicates"] = lineNum;
+		if (eachLine.find("functions")) partNum["functions"] = lineNum; //存储所有可能有用的行号,function就认为一个了
+		if (eachLine.find("action")) actionNum.push_back(lineNum);
+
+		allLine.push_back(eachLine);
+		lineNum++;
+		eachLine = "";
+	}
+}
+
+map<string, string> ReadFileClass::DealMostHeadPart()
+{
+	map<string, string>resultReturn;
+	smatch result;
+
+	regex_match(allLine[partNum["domain"]],result,regex("domain ([a-z]*)"));
+	resultReturn["domain"] = result[1];
+
+	regex_match(allLine[partNum["requirements"]], result, regex("([a-z,-]*)")); //注意这里匹配的不完全对
+
+	resultReturn["domain"] = result[1];
+
+	regex_match(allLine[partNum["domain"]], result, regex("domain ([a-z]*)"));
+	resultReturn["domain"] = result[1];
+
+	regex_match(allLine[partNum["domain"]], result, regex("domain ([a-z]*)"));
+	resultReturn["domain"] = result[1];
+	return resultReturn;
+}
+
+void ReadFileClass::DealPredicates(map<string, map<string, string>> &all)
+{
+
+}
+
+void ReadFileClass::DealAction(Action & action)
+{
+
 }
--- RWxml/ReadFile.h
+++ RWxml/ReadFile.h
@@ -3,27 +3,48 @@
 #include <fstream>
 #include <string>
 #include <regex>
+#include <vector>
+#include <map>
 using std::string;
+using std::vector;
+using std::map;
+
+struct Action
+{
+	string type;
+	string name;
+	map<string, string> param; //<变量名，变量类型>
+	bool isDurationNum;
+	string duration_Num;
+	Composite duration;
+	Composite condition;
+	Composite effect;
+};
+
+struct Composite
+{
+	string type;
+	string checkName;
+	map<string, string>param; //使用遍历方式即可
+};
 
 class ReadFileClass
 {
 public:
 	ReadFileClass(const char* path);
 	~ReadFileClass();
-	void DealWholePart();
-	string DealRequirements();
-	void DealTypes();
-	void DealPredicates();
+	void RealAll();
+	map<string,string> DealMostHeadPart();
+	void DealPredicates(map<string, map<string, string>> &all); //<变量名，变量类型>
+	void DealAction(Action &action);
 
 private:
 	string eachLine; //每一行的字符串
 	std::ifstream in; //输入流
 	string nextPath = ""; //可能多读一行，因此把这行留下
-	string* requirements;
-	string* types;
-
-
-
+	vector<string> allLine;
+	map<string, int> partNum;
+	vector<int> actionNum;
 
 	//每一行读取，读取之后就删除所有空格和备注
 	//然后执行连续匹配？直接就开始每一层匹配
