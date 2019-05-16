28a46da876e130a7f3c6777d5e5618b95af933be
lsylusiyao <lsylusiyao@qq.com>
2019-05-16 18:02:46 +0800

    update

--- RWxml/WriteConsole.h
+++ RWxml/WriteConsole.h
@@ -18,3 +18,26 @@ private:
 	void WriteAction(Action a,int numMain);
 	void WriteVariables(map<string, string> m, string headTab);
 };
+
+
+class WriteConsoleProblemClass
+{
+public:
+	WriteConsoleProblemClass(map<string, string> h,
+		map<string, string>o,
+		vector<ProblemInit1> vI,
+		vector<ProblemGoal1> vG,
+		vector<ProblemMetric1> vM);
+	void WriteHead2Console();
+	void WriteObjects2Console();
+	void WriteInit2Console();
+	void WriteGoal2Console();
+	void WriteMetric2Console();
+
+private:
+	map<string, string> head;
+	map<string, string> objects;
+	vector<ProblemInit1> vI,
+		vector<ProblemGoal1> vG,
+		vector<ProblemMetric1> vM);
+};
